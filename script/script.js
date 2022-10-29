518fff4f84c0c096ad8adf9f53923627bc5a839c
Chaion Saha <sahachaion2000@gmail.com>
2022-10-30 04:02:06 +0600

    all functionality added

--- script/script.js
+++ script/script.js
@@ -7,6 +7,7 @@ const pass = document.querySelector("#password");
 const conPass = document.querySelector("#confirmPassword");
 const passViewIcon = document.querySelector("#passwordView");
 const inputBoxes = document.querySelectorAll(".inputBox");
+const body = document.querySelector("body");
 
 document.addEventListener("click", (e) => {
 	if (pass === document.activeElement || pass.value !== "") {
@@ -28,14 +29,23 @@ document.addEventListener("click", (e) => {
 		let result = userName.value.split(" ").join("").match(/\W/gi);
 		if (result || userName.value === "") {
 			editElement(userName, "red", "Invalid Name");
+			body.classList.add("usernameInvalid");
 		} else {
 			userName.style.borderColor = "#57b846";
 			userName.parentElement.children[1].style.color = "#57b846";
 			userName.parentElement.children[1].style.borderColor = "#57b846";
 			userName.parentElement.children[1].innerText = "Name";
+			body.classList.remove("usernameInvalid");
 		}
 
 		logicWorks(email, "Email");
+		if (!email.checkValidity()) {
+			body.classList.add("EmailCheckInvalid");
+			editElement(email, "red", "Invalid Email");
+		} else {
+			body.classList.remove("EmailCheckInvalid");
+			editElement(email, "#57b846", "Email");
+		}
 
 		if (pass.value === "") {
 			editElement(pass, "red", "Invalid Password");
@@ -45,9 +55,25 @@ document.addEventListener("click", (e) => {
 			editElement(conPass, "#57b846", "Confirm Password");
 			if (pass.value !== conPass.value && conPass.value !== "") {
 				editElement(conPass, "red", "Password doesot match");
+				body.classList.add(`conpassInvalid`);
 			} else {
 				editElement(conPass, "#57b846", "Password Matches");
+				body.classList.remove(`conpassInvalid`);
 			}
+		}
+
+		if (
+			!body.classList.contains("usernameInvalid") &&
+			!body.classList.contains(`conpassInvalid`) &&
+			!body.classList.contains(`EmailInvalid`) &&
+			!body.classList.contains(`conpassInvalid`) &&
+			!body.classList.contains(`PasswordInvalid`) &&
+			!body.classList.contains(`EmailCheckInvalid`)
+		) {
+			document.querySelector(".form").innerHTML = ``;
+			document.querySelector(
+				".form"
+			).innerHTML = `<h3 class="signupConfirmation">Thanks for signing up.</h3>`;
 		}
 	}
 });
@@ -78,11 +104,13 @@ const editElement = function (nodeList, color, notificationText) {
 const logicWorks = function (nodeList, nodeName) {
 	if (nodeList.value === "") {
 		editElement(nodeList, "red", `Invalid ${nodeName}`);
+		body.classList.add(`${nodeName}Invalid`);
 	} else {
 		nodeList.style.borderColor = "#57b846";
 		nodeList.parentElement.children[1].style.color = "#57b846";
 		nodeList.parentElement.children[1].style.borderColor = "#57b846";
 		nodeList.parentElement.children[1].innerText = nodeName;
+		body.classList.remove(`${nodeName}Invalid`);
 	}
 };
 
